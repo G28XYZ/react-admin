0fa6df816820998d607136536fe00d0f82db995b
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-04-24 13:15:48 +0700

    feat: создание страниц по клику через php

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,30 @@
 import $ from "jquery";
 
-$.get(
-  "./api",
-  (data) => {
-    data.forEach((file) => {
-      $("body").append(`<h1>${file}</h1>`);
-    });
-  },
-  "JSON"
-);
+function getPageList() {
+  $("h1").remove();
+  $.get(
+    "./api",
+    (data) => {
+      data.forEach((file) => {
+        $("body").append(`<h1>${file}</h1>`);
+      });
+    },
+    "JSON"
+  );
+}
+
+getPageList();
+
+$("button").click(() => {
+  $.post(
+    "./api/createNewPage.php",
+    {
+      name: $("input").val(),
+    },
+    () => {
+      getPageList();
+    }
+  ).fail(() => {
+    alert("Страница уже существует");
+  });
+});
