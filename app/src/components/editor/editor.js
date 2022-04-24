b19008282773ca76dbcb574787ae3472ae5500bc
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-04-24 17:06:23 +0700

    feat: добавлен iframe

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import "../../helpers/iframeLoader.js";
 
 export default function Editor() {
+  let iframe;
+  const [currentPage, setCurrentPage] = useState("index.html");
   const [_state, setState] = useState({
     pageList: [],
     newPageName: "",
@@ -10,7 +13,7 @@ export default function Editor() {
   const _setState = (data) => setState((state) => ({ ...state, ...data }));
 
   useEffect(() => {
-    loadPageList();
+    init(currentPage);
     // return () => {
     //   cleanup
     // };
@@ -21,6 +24,19 @@ export default function Editor() {
       .get("./api")
       .then((res) => _setState({ pageList: res.data }))
       .catch(alert);
+  }
+
+  function init(page) {
+    iframe = document.querySelector("iframe");
+    open(page);
+    loadPageList();
+  }
+
+  function open(page) {
+    setCurrentPage(`../${page}`);
+    iframe.load(currentPage, () => {
+      console.log(currentPage);
+    });
   }
 
   function createNewPage() {
@@ -52,10 +68,11 @@ export default function Editor() {
   });
 
   return (
-    <>
-      <input onChange={(e) => _setState({ newPageName: e.target.value })} type="text" />
-      <button onClick={createNewPage}>Создать страницу</button>
-      {pages}
-    </>
+    <iframe src={currentPage} frameBorder={0}></iframe>
+    // <>
+    //   <input onChange={(e) => _setState({ newPageName: e.target.value })} type="text" />
+    //   <button onClick={createNewPage}>Создать страницу</button>
+    //   {pages}
+    // </>
   );
 }
