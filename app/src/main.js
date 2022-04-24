ea52d88e05d4aa1035394f9e4b7e1474916bfe53
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-04-24 13:02:02 +0700

    feat: первое взаимодействие с php файлом - чтение файлов

--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,11 @@
 import $ from "jquery";
 
-$("h1").fadeOut();
+$.get(
+  "./api",
+  (data) => {
+    data.forEach((file) => {
+      $("body").append(`<h1>${file}</h1>`);
+    });
+  },
+  "JSON"
+);
